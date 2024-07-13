02da26c56a40ec7ad6ced48fddfec30be19b2856
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2024-07-13 01:18:03 +0000

    Test

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module ghubdep
 
 go 1.22.4
+
+require github.com/google/go-github/v63 v63.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/google/go-github/v63/github"
 )
 
+var client *github.Client
+
 func main() {
+	client = github.NewClient(nil)
 	http.HandleFunc("/", eventHandler)
 	log.Fatalln(http.ListenAndServe(":9922", nil))
 }
@@ -32,11 +38,37 @@ func eventHandler(w http.ResponseWriter, r *http.Request) {
 
 		switch strings.ToLower(r.Header.Get("x-github-event")) {
 		case "pull_request":
-			_, _ = w.Write([]byte("A pull request was merged! A deployment should start now..."))
-			return
+			if payload["action"].(string) == "closed" && payload["pull_request"].(map[string]any)["merged"].(bool) {
+				startDeployment(payload["pull_request"].(map[string]any))
+				_, _ = w.Write([]byte("A pull request was merged! A deployment should start now..."))
+				return
+			}
 		}
 
 		_, _ = w.Write([]byte("OK"))
 	}
 	_, _ = w.Write([]byte("Hello World!"))
+}
+
+func startDeployment(pullRequest map[string]any) {
+	user := pullRequest["user"].(map[string]any)["login"].(string)
+	repo := pullRequest["repository"].(map[string]any)["full_name"].(string)
+	ref := pullRequest["head"].(map[string]any)["sha"].(string)
+	env := "dev"
+	desc := "Just a deployment"
+
+	deployment, res, err := client.Repositories.CreateDeployment(
+		context.Background(),
+		user,
+		repo,
+		&github.DeploymentRequest{Ref: &ref, Environment: &env, Description: &desc},
+	)
+
+	if err != nil {
+		log.Println(err)
+		log.Println(res.StatusCode)
+		return
+	}
+
+	log.Println(deployment)
 }
